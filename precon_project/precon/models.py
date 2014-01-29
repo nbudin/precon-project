1c3b9e42040443d4c36de05d80f1670eb5bf3d4e
Kevin Riggle <kevinr@free-dissociation.com>
2014-01-29 03:50:11 -0500

    remove unique name/email to save hassle

--- precon_project/precon/models.py
+++ precon_project/precon/models.py
@@ -10,8 +10,8 @@ class Participant(models.Model):
     modification_time = models.DateTimeField(auto_now=True, editable=False)
     nonce = models.CharField(default=lambda: id_generator(size=6), unique=True, editable=False, max_length=6)
 
-    name = models.CharField("Your name, as you would like it to appear in any published material", max_length=50, unique=True)
-    email = models.EmailField(max_length=50, unique=True)
+    name = models.CharField("Your name, as you would like it to appear in any published material", max_length=50)
+    email = models.EmailField(max_length=50)
     phone = models.CharField("Phone number", max_length=15, null=True, blank=True, help_text="Not for use in scheduling, but so we have it on record, so we can get in touch with you at the con if we need to for some reason. (Eg. you aren't at your panel and it's ten minutes past the scheduled start time.)")
     panel_proposals_responded = models.ManyToManyField('PanelProposal', through='PanelProposalResponse', related_name='participants_responded', null=True, blank=True)
     slots_available = models.ManyToManyField('Slot', verbose_name="At which of these times would you be available **and happy** to sit on panels?", related_name='participants_available', null=True, blank=True)
